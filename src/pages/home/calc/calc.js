561ce0b3965a9f0a9746c53d2b594a097a1357ec
vladyslavketov <106628608+vladyslavketov@users.noreply.github.com>
2023-03-29 12:31:00 +0300

    Update calc.js

--- src/pages/home/calc/calc.js
+++ src/pages/home/calc/calc.js
@@ -26,7 +26,7 @@ function onFirstLoad() {
     src="${imgSrc}"
     alt="${imgAlt}"
   />`;
-  
+
   calcFormRefs.titleCategory.innerText = text;
   setIsHiddenClass('.calc-form__category-list', `[data-id="${id}"]`);
 }
@@ -70,7 +70,7 @@ function onCalcFormChange(e) {
   }
 }
 
-export default function setIsHiddenClass(querySelector, attr) {
+function setIsHiddenClass(querySelector, attr) {
   const currentEl = document.querySelector(`${querySelector}${attr}`);
   const elList = document.querySelectorAll(querySelector);
 
@@ -79,34 +79,4 @@ export default function setIsHiddenClass(querySelector, attr) {
   }
 
   if (currentEl) currentEl.classList.remove('isHidden');
-}
-
-export default function catImgMarkup(data) {
-  if (!data) return;
-
-  const { articleId, title, price, img } = data;
-
-  return `
-    <li class="cart-products__item">
-      <article class="product-card" data-id="${articleId}">
-        <img class="product-card__img" src="${img}" alt="${title}" width="320" height="240">
-        <div class="product-card__desc">
-          <h3 class="product-card__title">${title}</h3>
-        </div>
-        <div class="product-card__counter counter">
-          <button class="counter__control" data-action="decrement" disabled>-</button>
-          <p class="counter__value">1</p>
-          <button class="counter__control" data-action="increment">+</button>
-        </div>
-        <div class="product-card__cost-wrap">
-          <p class="product-card__cost" data-price="${price}">${price}</p>
-          <span>грн</span>
-        </div>
-        <button class="product-card__btn--delete modal__close-btn" type="button" aria-label="Кнопка видалення картки товару">
-          <span class="burger-line"></span>
-          <span class="burger-line"></span>
-        </button>
-      </article>
-    </li>
-  `;
 }
